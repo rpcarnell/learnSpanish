90a1c79a704a5636e6d8d167812dbd13739353af
root <root@rpcarnell-MS-7529>
2016-09-13 23:59:54 -0500

    September 13 changes

--- application/views/front/profile/emailtutor.php
+++ application/views/front/profile/emailtutor.php
@@ -2,18 +2,18 @@
 
 ?><div class="spanish">
     <div class="spanish-content profile">
-        <?php echo $breadcrumbs; ?><br /><br /><br />
+   
 
-<div style="margin-top: 40px;">
+<div style="margin-top: 20px;">
     <?php 
     if (is_array($tutors)) {
         echo "<h2>Your Tutors:</h2>";
     foreach ($tutors  as $recu)
     {
-      
+      echo "<div style='border-bottom: 1px solid #aaa; margin-bottom: 30px; padding-bottom: 10px;'>";
         $calendar = $this->Customers_model->getCalendarEntry($recu->customer_ID, $recu->tutor_id, $recu->from_recurring);
         $skype = ($recu->skype_name) ? " | <b>Skype:</b> $recu->skype_name" : '' ;
-        echo "<p><div style='padding: 2px; background: #f0f0f0; margin: 0px; margin-bottom: 5px;'><a href='".base_url()."calendar/tutorSchedule/".$recu->tutor_id."'>".$recu->name."</a> | <b>E-Mail:</b> ".$recu->email_list." ".$skype."</div>";
+        echo "<p><div style='padding: 10px; background: #f0f0f0; margin: 0px; margin-bottom: 5px;'><a style='text-decoration: none;' href='".base_url()."calendar/tutorSchedule/".$recu->tutor_id."'>".$recu->name."</a> | <b>E-Mail:</b> ".$recu->email_list." ".$skype."</div>";
         if ($calendar->from_recurring == 0)
         {
             echo "You have a Flex Time appointment with this tutor at ".date('M-d-Y G:i:s', $calendar->unix_time);
@@ -23,6 +23,7 @@
             echo "You have a Recurring Appointment with this tutor";
         }
         echo "</p>";
+        echo "</div>";
     }} else echo "You have no calendar entries yet";
     ?>
     <?php 
